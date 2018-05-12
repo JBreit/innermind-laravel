9291a9d2400ac519ec33139655b74d2ffb4d2ec5
Jason Breitigan <jason.breitigan@gmail.com>
2018-05-11 20:25:29 -0400

    Edited app.js file.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -21,42 +21,40 @@ const app = new Vue({
     el: '#app'
 });
 
-// ((document) => {
-//   const toggler = document.querySelector('[data-toggle="push"]');
-//   const target = toggler.dataset.target || toggler.attribute.href || '#sidebar';
-//   const direction = toggler.dataset.direction || 'right';
-//   const body = toggler.dataset.canvas || document.getElementsByTagName('body')[0];
+((document) => {
+  const toggler = document.querySelector('[data-toggle="push"]');
+  const target = toggler.dataset.target || toggler.attribute.href || '#sidebar';
+  const direction = toggler.dataset.direction || 'right';
+  const body = toggler.dataset.canvas || document.getElementsByTagName('body')[0];
 
-//   body.classList.add('push-canvas');
+  body.classList.add('push-canvas');
 
-//   const toggleClass = (element, direction) => {
+  const toggleClass = (element, direction) => {
 
-//     if (element.classList) {
-//       element.classList.toggle(`pushed-${direction}`);
-//     }
+    if (element.classList) {
+      element.classList.toggle(`pushed-${direction}`);
+    }
 
-//     const classes = element.className.split(' ');
-//     const index = classes.indexOf(direction);
+    const classes = element.className.split(' ');
+    const index = classes.indexOf(direction);
 
-//     if (index >= 0) {
-//       classes.splice(index, 1);
-//     }
+    if (index >= 0) {
+      classes.splice(index, 1);
+    }
 
-//     element.className = classes.join(' ');
-//   };
+    element.className = classes.join(' ');
+  };
 
-//   const toggleExpanded = (selector, closed, open) => {
-//     const element = document.querySelector(selector);
+  const toggleExpanded = (selector, closed, open) => {
+    const element = document.querySelector(selector);
 
-//     element.setAttribute('aria-expanded', element.getAttribute('aria-expanded') === closed ? open : closed);
-//   };
+    element.setAttribute('aria-expanded', element.getAttribute('aria-expanded') === closed ? open : closed);
+  };
 
-//   toggler.addEventListener('click', (event) => {
-//     event.preventDefault();
-//     toggleClass(body, direction);
-//     toggleExpanded('#sidebar', 'false', 'true');
-//   }, false);
+  toggler.addEventListener('click', (event) => {
+    event.preventDefault();
+    toggleClass(body, direction);
+    toggleExpanded('#sidebar', 'false', 'true');
+  }, false);
 
-// })(document);
-
-require('./push-navigation');
+})(document);
